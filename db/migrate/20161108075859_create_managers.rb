94f51e36e7462a24b6d6c3dbc41050f84132a059
Prima Aulia Gusta <primaulia@gmail.com>
2016-11-08 16:02:52 +0800

    new managers class

---
+++ db/migrate/20161108075859_create_managers.rb
@@ -0,0 +1,9 @@
+class CreateManagers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :managers do |t|
+      t.string :name
+      t.string :email
+      t.string :office_number
+    end
+  end
+end
